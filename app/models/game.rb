cd7d9cef2a2fe04b97796f5d9fa600edfd8a0a3a
Mario Hilsenrath <mario.hilsenrath@gmail.com>
2018-11-27 11:55:30 +0100

    seeds create and dependent destroy true

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,6 +2,6 @@ class Game < ApplicationRecord
   belongs_to :team_a, class_name: 'Team'
   belongs_to :team_b, class_name: 'Team'
   belongs_to :league
-  has_many :chats
+  has_many :chats, dependent: :destroy
   validates :kick_off_time, presence: true
 end
