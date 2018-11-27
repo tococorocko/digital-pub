35b7d9de4ae6bd363c5ccde3935a3ae540786224
Amelie Boudier <amelie.boudier01@gmail.com>
2018-11-27 18:28:14 +0100

    created show view of chat

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :user_chats, dependent: :destroy
+  has_many :messages, through: :user_chats
+  has_many :chats, through: :user_chats
   has_many :favorite_leagues, dependent: :destroy
   has_many :favorite_teams, dependent: :destroy
   validates :email, presence: true, uniqueness: true
   validates :username, presence: true, uniqueness: true
+
+  def user_chat_for(a_chat)
+    user_chats.where(chat: a_chat).first
+  end
 end
