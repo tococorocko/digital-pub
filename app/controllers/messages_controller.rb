156522f754fff28907959f71f5aea501568f7589
Amelie Boudier <amelie.boudier01@gmail.com>
2018-11-28 14:29:16 +0100

    created actioncable chat

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,16 +1,25 @@
 class MessagesController < ApplicationController
   def create
     @message = Message.new(params_message)
-    @user_chat = UserChat.find(params[:user_chat_id])
-    @message.user_chat = @user_chat
-    @message.save!
-    @chat = @user_chat.chat
-    redirect_to game_chat_path(@chat.game, @chat)
+    @chat = Chat.find(params[:chat_id])
+    @message.chat = @chat
+    @message.user = current_user
+    if @message.save
+      respond_to do |format|
+        format.html { redirect_to game_chat_path(@chat) }
+        format.js
+      end
+    else
+      respond_to do |format|
+        format.html { render "chats/show" }
+        format.js
+      end
+    end
   end
 
   private
 
   def params_message
-    params.require(:message).permit(:content, :user_chat_id)
+    params.require(:message).permit(:content)
   end
 end
