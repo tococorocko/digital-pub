f330b90368e9ef2b15cc3596f8664d282a80b663
Mario Hilsenrath <mario.hilsenrath@gmail.com>
2018-11-29 17:00:30 +0100

    link to chats

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -4,14 +4,10 @@ class GamesController < ApplicationController
   def index
     @date = Date.today
     @time = Time.now.utc
-    @games = Game.all
     @user = current_user
-    @league = League.first
 
-    # if current_user.present? && current_user.favorite_leagues.any?
-    #   @league = @user.favorite_leagues.first
-    # else
-    #   @league = League.first
-    # end
+    @league = League.find(params[:league]) || League.first
+
+    @games = Game.where(league: @league)
   end
 end
