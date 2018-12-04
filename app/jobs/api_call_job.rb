2b5fd28105d8df1086a310e6c364dea38b7baddd
Daniel Trujillo <ganimedes@firemail.cc>
2018-12-04 18:15:04 +0100

    added verification to apicalljob

--- app/jobs/api_call_job.rb
+++ app/jobs/api_call_job.rb
@@ -7,12 +7,16 @@ class ApiCallJob < ApplicationJob
     list = RestClient.get "https://api.football-data.org/v2/competitions/#{league}/matches?status=SCHEDULED", {'X-Auth-Token' => ENV['FOOTBALL_DATA_TOKEN'] }
     games = JSON.parse(list)
     games['matches'].first(num).each do |game|
-      Game.create(
-        team_a_id: Team.find_by(name: game["homeTeam"]["name"]).id,
-        team_b_id: Team.find_by(name: game["awayTeam"]["name"]).id,
-        kick_off_time: game["utcDate"],
-        league_id: identifier
-        )
+      unless Game.find_by(team_a_id: Team.find_by(name: game["homeTeam"]["name"]).id,
+                          team_b_id: Team.find_by(name: game["awayTeam"]["name"]).id,
+                          kick_off_time: game["utcDate"])
+        Game.create(
+          team_a_id: Team.find_by(name: game["homeTeam"]["name"]).id,
+          team_b_id: Team.find_by(name: game["awayTeam"]["name"]).id,
+          kick_off_time: game["utcDate"],
+          league_id: identifier
+          )
+      end
     end
   end
 end
